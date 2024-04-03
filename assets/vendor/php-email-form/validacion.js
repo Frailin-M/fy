da2826fe15c2e81b5bc836fd2d58938b7631c0c5
Frailin M <frailinm8@gmail.com>
2024-04-03 18:57:06 -0400

    Segundo commit

---
+++ assets/vendor/php-email-form/validacion.js
@@ -0,0 +1,67 @@
+(function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach(function(form) {
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      let thisForm = this;
+
+      // Mostrar el círculo de carga
+      let loadingCircle = thisForm.querySelector('.loading');
+      loadingCircle.style.display = 'block';
+
+      // Deshabilitar botones durante el envío del formulario
+      let submitButton = thisForm.querySelector('button[type="submit"]');
+      let resetButton = thisForm.querySelector('button[type="reset"]');
+      submitButton.disabled = true;
+      resetButton.disabled = true;
+
+      // Simular el envío del formulario
+      setTimeout(function() {
+        // Simular éxito
+        loadingCircle.style.display = 'none';
+        thisForm.querySelector('.sent-message').classList.add('show');
+        fadeIn(thisForm.querySelector('.sent-message'));
+        // Limpiar formulario
+        thisForm.reset();
+
+        // Restaurar botones a su posición original
+        submitButton.disabled = false;
+        resetButton.disabled = false;
+
+        // Después de 3 segundos, ocultar el mensaje de éxito
+        setTimeout(function() {
+          thisForm.querySelector('.sent-message').classList.remove('show');
+        }, 3000);
+      }, 1500);
+    });
+  });
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').style.display = 'none';
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+  // Función para desvanecer gradualmente un elemento
+  function fadeOut(element) {
+    element.style.transition = 'opacity 1s ease-in-out';
+    element.style.opacity = 0;
+    setTimeout(function() {
+      element.style.display = 'none';
+    }, 1000);
+  }
+
+  // Función para hacer aparecer gradualmente un elemento
+  function fadeIn(element) {
+    element.style.display = 'block';
+    setTimeout(function() {
+      element.style.transition = 'opacity 1s ease-in-out';
+      element.style.opacity = 1;
+    }, 100);
+  }
+
+})();
+
